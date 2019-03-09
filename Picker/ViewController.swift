f4c3a212573143f7ed835e3e4f037afa170034b0
aki0207 <forever.look.aki@docomo.ne.jp>
2019-03-09 16:53:20 +0900

    ピッカー触ってみた

--- Picker/ViewController.swift
+++ Picker/ViewController.swift
@@ -1,20 +1,55 @@
-//
-//  ViewController.swift
-//  Picker
-//
-//  Created by ききょ on 2019/03/09.
-//  Copyright © 2019 kikyo. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIPickerViewDelegate, UIPickerViewDataSource {
+    
 
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    let dataList = [
+        "りんご","バナナ","ぶどう","もも",
+        "みかん","パイナップル","マンゴー","レモン",
+        "キウイ","メロン","スイカ","ゴリラ"
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Delegate設定
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        label.text = "フルーツカーニバル"
+        
     }
-
-
+    
+    // UIPickerViewの列の数
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // UIPickerViewの行数、リストの数
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int) -> Int {
+        return dataList.count
+    }
+    
+    // UIPickerViewの最初の表示
+    func pickerView(_ pickerView: UIPickerView,
+                    titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        
+        return dataList[row]
+    }
+    
+    // UIPickerViewのRowが選択された時の挙動
+    func pickerView(_ pickerView: UIPickerView,
+                    didSelectRow row: Int,
+                    inComponent component: Int) {
+        
+        label.text = dataList[row]
+        
+    }
+    
 }
-
